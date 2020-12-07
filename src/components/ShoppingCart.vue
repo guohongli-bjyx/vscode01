49f81dea8b331a321d3cbd905fbd21f2780a613e
guohongli <2389704865@qq.com>
2020-12-07 21:48:58 +0800

    更改

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,7 +3,7 @@
         <p v-show="!lists.length">请添加产品到购物车</p>
          
         <li v-for="(list,index) in lists" :key="index">{{list.title}} - {{list.price}} x {{count}}</li>
-        <!-- 如何去掉开始的那个点  如何在再次点购买时不替换掉前一个 为什么在cart.js里 stste.list=product就会出现一个点 而且后一个会覆盖前一个-->
+        <!-- 如何去掉开始的那个点  如何在再次点购买时不替换掉前一个 为什么在cart.js里 stste.list=product就会出现一个点 而且后一个会覆盖前一个呢-->
         合计:
 
     </div>
